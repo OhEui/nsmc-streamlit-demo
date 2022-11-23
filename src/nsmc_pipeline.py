0ddaeb9d66c927bdc1163914a95978c789029305
EuiTaek <bluedog1881@gmail.com>
2022-11-23 17:01:34 +0900

    chor : add commnet for get_nsmc_pipeline func

--- src/nsmc_pipeline.py
+++ src/nsmc_pipeline.py
@@ -4,6 +4,20 @@ def get_nsmc_pipeline(
     model_name_or_path = "monologg/koelectra-v3-small-nsmc",
     device= 0,
     ):
+    """
+    NSMC Pipeline
+
+    Args:
+        model_name_or_path (str, optional): 
+        model name (hugging face)or local path. 
+        Defaults to "monologg/koelectra-v3-small-nsmc".
+        device (int, optional): 
+        -1 for cpu . 0>= for gpu (gpu id)
+        Defaults to 0.
+
+    Returns:
+        TextClassificationPipeline
+    """
     model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
     tokenizer =AutoTokenizer.from_pretrained(model_name_or_path)
 
