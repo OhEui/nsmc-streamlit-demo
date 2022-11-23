f247dd51a97549adabb117051fa56d3aa16a6ddb
EuiTaek <bluedog1881@gmail.com>
2022-11-23 16:40:40 +0900

    feat : nsmc pipeline

---
+++ src/nsmc_pipeline.py
@@ -0,0 +1,13 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+def get_nsmc_pipeline():
+    model = AutoModelForSequenceClassification.from_pretrained("monologg/koelectra-v3-small-nsmc")
+    tokenizer =AutoTokenizer.from_pretrained("monologg/koelectra-v3-small-nsmc")
+
+    pipe = TextClassificationPipeline(
+    model = model,
+    tokenizer = tokenizer,
+    device = 0,
+    )
+
+    return pipe
